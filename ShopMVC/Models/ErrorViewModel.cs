213870b1398f7bba6fb58ffd604b9b9f058eced3
marishik <m.g.alexeeva@bk.ru>
2024-07-15 12:21:22 +0500

    Add project files.

---
+++ ShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ShopMVC.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
