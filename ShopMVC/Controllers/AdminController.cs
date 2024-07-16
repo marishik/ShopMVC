0566e5e3df133053b8e48010610f5273628e0023
CMep4 <karashutovm@mail.ru>
2024-07-16 09:08:12 +0600

    Added form to add person to database
    Added 2 pages to new admin controller. Page person add has form to add person. Index is default page for admin

---
+++ ShopMVC/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.Client;
+
+namespace ShopMVC.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private static HttpClient _client = new();
+    private PracticeClient _practiceClient = new("http://192.168.0.101:5064", _client); //URL NEEDS TO REF TO API ADDRESS
+
+    public AdminController(ILogger<HomeController> logger) {
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult PersonAdd()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult PersonAdd(Person person)
+    {
+        _practiceClient.PostPersonAsync(person);
+        return View();
+    }
+}
