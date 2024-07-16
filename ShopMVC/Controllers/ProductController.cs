e89de9e87ea5fb70b72fd45f55d094b6f6da101f
NBirmanov <nurbolbirmanov7@gmail.com>
2024-07-16 15:08:45 +0500

    Product CRUD added

---
+++ ShopMVC/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.Client;
+
+namespace ShopMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        static HttpClient _httpClient = new HttpClient();
+        static PracticeClient _practiceClient = new PracticeClient("http://192.168.98.78:5064/", _httpClient);
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var product = _practiceClient.GetProductAsync().Result.Products;
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult Create(Product product)
+        {
+            _practiceClient.PostProductAsync(product);
+
+            return Redirect("/Product/Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var products = _practiceClient.GetProductAsync().Result.Products;
+            foreach (var product in products) {
+                if (product.Id == id)
+                {
+                    return View(product);
+                }
+            }
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public RedirectResult Edit(Product product)
+        {
+            _practiceClient.PutProductAsync(product);
+
+            return Redirect("/Product/Index");
+        }
+    }
+}
